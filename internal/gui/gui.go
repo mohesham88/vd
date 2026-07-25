3b25daf1eec77e6871155b71a8a10c19e55100a3
Ahmed Hossam <ahmedhosssamps@gmail.com>
2026-07-25 17:23:38 +0300

    Building foundation

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -9,45 +9,31 @@ import (
 	"github.com/awesome-gocui/gocui"
 
 	"github.com/ahmedhosssam/vd/internal/app"
-	"github.com/ahmedhosssam/vd/internal/fuzzy"
 )
-
-const searchBarView = "searchbar"
-
-const passphraseView = "passphrase"
-
-const searchBarContentRows = 1
-
-const searchBarMiddleRow = searchBarContentRows / 2
 
 const (
-	barContentRows = 1
-	barWidth       = 60
+	GpgPassphraseView = "gpgpassphraseview"
+	PasswordsView     = "passwordsview"
 )
 
-const maxRows = 20
-
-var numRows = 0
-
-var currentRow = -1
-
-var gui *gocui.Gui
-
-var passwords []string
-
-var commands = []string{"/add", "/change", "/delete", "/gen"}
-
 var (
-	locked              bool
-	passphraseMsg       string
-	pendingPasswordCopy string
+	gg            bool
+	passphraseMsg string
+	viewStack     []string
+	gui           *gocui.Gui
 )
 
 func Run() {
 	app.NoTerminalPrompt = true
 
-	passwords = app.ReadPasswordsLookup()
-	locked = passwords == nil
+	passwords := app.ReadPasswordsLookup()
+	locked := passwords == nil
+
+	viewStack = append(viewStack, PasswordsView)
+
+	if locked {
+		viewStack = append(viewStack, GpgPassphraseView)
+	}
 
 	g, err := gocui.NewGui(gocui.OutputNormal, true)
 	if err != nil {
@@ -55,11 +41,9 @@ func Run() {
 	}
 	defer g.Close()
 
-	g.Highlight = true
-	g.Cursor = true
-	g.SelFgColor = gocui.ColorCyan
-
 	gui = g
+
+	g.Cursor = true
 
 	g.SetManagerFunc(layout)
 
@@ -67,125 +51,70 @@ func Run() {
 		log.Panicln(err)
 	}
 
-	if err := g.SetKeybinding(searchBarView, gocui.KeyEnter, gocui.ModNone, copyFirstRowAndQuit); err != nil {
+	if err := g.SetKeybinding("", gocui.KeyCtrlG, gocui.ModNone, changeLayout); err != nil {
 		log.Panicln(err)
 	}
 
-	if err := g.SetKeybinding(passphraseView, gocui.KeyEnter, gocui.ModNone, unlock); err != nil {
-		log.Panicln(err)
-	}
-
-	for i := range maxRows {
-		if err := g.SetKeybinding(barViewName(i), gocui.KeyEnter, gocui.ModNone, copyRowAndQuit); err != nil {
-			log.Panicln(err)
-		}
-	}
-	if err := g.SetKeybinding("", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
+	if err := g.SetKeybinding(GpgPassphraseView, gocui.KeyEnter, gocui.ModNone, unlock); err != nil {
 		log.Panicln(err)
 	}
 
 	if err := g.MainLoop(); err != nil && !errors.Is(err, gocui.ErrQuit) {
 		log.Panicln(err)
 	}
-
-	if v, err := g.View(searchBarView); err == nil && v != nil {
-		fmt.Printf("Search: %q\n", strings.TrimSpace(v.ViewBuffer()))
-	}
 }
 
 func layout(g *gocui.Gui) error {
-	if locked {
-		return passphraseLayout(g)
-	}
+	topStack := viewStack[len(viewStack)-1]
 
+	switch topStack {
+	case PasswordsView:
+		return passwordsLayout(g)
+	case GpgPassphraseView:
+		return gpgPassphraseLayout(g)
+	default:
+		return nil
+	}
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func changeLayout(g *gocui.Gui, v *gocui.View) error {
+	gg = !gg
+	return nil
+}
+
+func gpgPassphraseLayout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	w := barWidth
-	h := searchBarContentRows + 1
+	w := 60
+	h := 2
 	x0 := (maxX - w) / 2
 	y0 := (maxY - h) / 2
 	x1 := x0 + w
 	y1 := y0 + h
 
-	v, err := g.SetView(searchBarView, x0, y0, x1, y1, 0)
+	v, err := g.SetView(GpgPassphraseView, x0, y0, x1, y1, 0)
 	if err != nil {
 		if !errors.Is(err, gocui.ErrUnknownView) {
 			return err
 		}
-		v.Editable = true
-		v.Wrap = false
-		v.Editor = gocui.EditorFunc(oneLineEditor)
-		if err := v.SetCursorUnrestricted(0, searchBarMiddleRow); err != nil {
-			return err
-		}
-		if _, err := g.SetCurrentView(searchBarView); err != nil {
-			return err
-		}
-	}
 
-	barH := barContentRows + 1
-	if err := renderRows(g, x0, y1, x1, barH); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func passphraseLayout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	w := barWidth
-	h := searchBarContentRows + 1
-	x0 := (maxX - w) / 2
-	y0 := (maxY - h) / 2
-
-	v, err := g.SetView(passphraseView, x0, y0, x0+w, y0+h, 0)
-	if err != nil {
-		if !errors.Is(err, gocui.ErrUnknownView) {
-			return err
-		}
 		v.Editable = true
 		v.Wrap = false
 		v.Mask = '*'
 		v.Editor = gocui.EditorFunc(passphraseEditor)
-		if _, err := g.SetCurrentView(passphraseView); err != nil {
+
+		if _, err := g.SetCurrentView(GpgPassphraseView); err != nil {
 			return err
 		}
+
 	}
 
-	v.Title = " GPG passphrase "
+	v.Title = " Enter GPG Passphrase "
 	if passphraseMsg != "" {
-		v.Title = " " + passphraseMsg + " "
-	}
-
-	return nil
-}
-
-func unlock(g *gocui.Gui, v *gocui.View) error {
-	app.PassphraseCache = strings.TrimRight(v.Buffer(), "\r\n")
-
-	passwords = app.ReadPasswordsLookup()
-	if passwords == nil {
-		app.PassphraseCache = ""
-		passphraseMsg = "wrong passphrase, try again"
-		v.Clear()
-		if err := v.SetOrigin(0, 0); err != nil {
-			return err
-		}
-		return v.SetCursorUnrestricted(0, 0)
-	}
-
-	locked = false
-	if err := g.DeleteView(passphraseView); err != nil {
-		return err
-	}
-
-	if pendingPasswordCopy != "" {
-		name := pendingPasswordCopy
-		pendingPasswordCopy = ""
-		app.GetPassword(name)
-		return gocui.ErrQuit
+		v.Title = fmt.Sprintf(" %s ", passphraseMsg)
 	}
 	return nil
 }
@@ -204,199 +133,67 @@ func passphraseEditor(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier)
 	gocui.DefaultEditor.Edit(v, key, ch, mod)
 }
 
-func renderRows(g *gocui.Gui, x0, y1, x1, barH int) error {
-	sv, err := g.View(searchBarView)
-	if err != nil {
-		return nil
-	}
-	pattern := strings.TrimSpace(sv.ViewBuffer())
-	entries := passwords
-
-	if strings.HasPrefix(pattern, "/") {
-		entries = commands
-	}
-
-	matches := fuzzy.Search(pattern, entries)
-	numRows = len(matches)
-
-	for i := 0; i < numRows; i++ {
-		by0 := y1 + 1 + i*barH
-		by1 := by0 + barH
-
-		bv, err := g.SetView(barViewName(i), x0, by0, x1, by1, 0)
-		isNew := errors.Is(err, gocui.ErrUnknownView)
-		if err != nil && !isNew {
-			return err
-		}
-		bv.Wrap = false
-
-		if isNew {
-			bv.BgColor = gocui.ColorWhite
-			bv.FgColor = gocui.ColorBlack
-		}
-
-		hotRow := currentRow
-		if currentRow == -1 {
-			hotRow = 0
-		}
-
-		if i == hotRow {
-			bv.Frame = true
-		} else {
-			bv.Frame = false
-		}
-
-		bv.Clear()
-		fmt.Fprint(bv, matches[i].Str)
-	}
-
-	// Delete stale bar views beyond the current match count.
-	for i := numRows; i < maxRows; i++ {
-		if _, err := g.View(barViewName(i)); err == nil {
-			if err := g.DeleteView(barViewName(i)); err != nil {
-				return err
-			}
-		}
-	}
-
-	if currentRow >= numRows {
-		_ = setRowView(g, -1)
-	}
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
-}
-
-func barViewName(i int) string {
-	return fmt.Sprintf("bar%d", i)
-}
-
-func setRowView(g *gocui.Gui, i int) error {
-	if locked || i < -1 || i >= numRows {
-		return nil
-	}
-	if currentRow >= 0 {
-		if old, err := g.View(barViewName(currentRow)); err == nil {
-			old.Frame = false
-		}
-	}
-	currentRow = i
-	if i >= 0 {
-		if nv, err := g.View(barViewName(i)); err == nil {
-			nv.Frame = true
-		}
-	}
-	_, err := g.SetCurrentView(searchBarView)
-	return err
-}
-
-func cursorUp(g *gocui.Gui, v *gocui.View) error {
-	return setRowView(g, currentRow-1)
-}
-
-func cursorDown(g *gocui.Gui, v *gocui.View) error {
-	next := currentRow + 1
-	if currentRow == -1 {
-		next = 1
-	}
-	return setRowView(g, next)
-}
-
-func copyRowAndQuit(g *gocui.Gui, v *gocui.View) error {
-	if v == nil {
-		return gocui.ErrQuit
-	}
-
-	rowText := strings.TrimSpace(v.ViewBuffer())
-
-	if rowText == "/gen" {
-		return handleGenCommand()
-	}
-
-	if app.ReadPasswordsLookup() == nil {
-		pendingPasswordCopy = rowText
-		locked = true
-		passphraseMsg = ""
-
-		for i := range maxRows {
-			if _, err := g.View(barViewName(i)); err == nil {
-				_ = g.DeleteView(barViewName(i))
-			}
-		}
-		if _, err := g.View(searchBarView); err == nil {
-			_ = g.DeleteView(searchBarView)
-		}
-		return nil
-	}
-
-	app.GetPassword(rowText)
-	return gocui.ErrQuit
-}
-
-func handleGenCommand() error {
-	newPassword, err := app.GenerateRandomPassword()
-	if err != nil {
-		return err
-	}
-
-	if err := app.CopyToClipboard(newPassword); err != nil {
-		return err
-	}
-
-	fmt.Printf("New random password copied to clipboard\n")
-	return gocui.ErrQuit
-}
-
-func copyFirstRowAndQuit(g *gocui.Gui, v *gocui.View) error {
-	if numRows == 0 {
-		return nil
-	}
-
-	row := currentRow
-	if row < 0 {
-		row = 0
-	}
-
-	bv, err := g.View(barViewName(row))
-	if err != nil {
-		return nil
-	}
-
-	return copyRowAndQuit(g, bv)
-}
-
-func oneLineEditor(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+func passwordsSearchEditor(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
 	switch key {
-	case gocui.KeyEnter, gocui.KeyCtrlJ: // ignore newline insertion
+	case gocui.KeyEnter, gocui.KeyCtrlJ:
 		return
 	case gocui.KeyArrowUp, gocui.KeyArrowDown:
 		return
 	case gocui.KeyBackspace, gocui.KeyBackspace2:
-		cx, _ := v.Cursor()
-		if cx == 0 {
+		if cx, _ := v.Cursor(); cx == 0 {
 			return
 		}
 	}
 	gocui.DefaultEditor.Edit(v, key, ch, mod)
+}
 
-	cx, cy := v.Cursor()
+func unlock(g *gocui.Gui, v *gocui.View) error {
+	app.PassphraseCache = strings.TrimRight(v.Buffer(), "\r\n")
+	passwords := app.ReadPasswordsLookup()
 
-	gui.Update(func(g *gocui.Gui) error {
-		if err := layout(g); err != nil {
+	if passwords == nil {
+		app.PassphraseCache = ""
+		v.Clear()
+		passphraseMsg = " Wrong passphrase, try again "
+		if err := v.SetOrigin(0, 0); err != nil {
 			return err
 		}
-		if _, err := g.SetCurrentView(searchBarView); err != nil {
-			return err
-		}
-		if sv, err := g.View(searchBarView); err == nil {
-			_ = sv.SetCursorUnrestricted(cx, searchBarMiddleRow)
-		}
-		return nil
-	})
-
-	if cy != searchBarMiddleRow {
-		_ = v.SetCursorUnrestricted(cx, searchBarMiddleRow)
+		return v.SetCursorUnrestricted(0, 0)
 	}
+
+	viewStack = viewStack[:len(viewStack)-1]
+	passphraseMsg = ""
+
+	if err := g.DeleteView(GpgPassphraseView); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func passwordsLayout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	w := 60
+	h := 2
+	x0 := (maxX - w) / 2
+	y0 := (maxY - h) / 2
+	x1 := x0 + w
+	y1 := y0 + h
+
+	v, err := g.SetView(PasswordsView, x0, y0, x1, y1, 0)
+	if err != nil {
+		if !errors.Is(err, gocui.ErrUnknownView) {
+			return err
+		}
+
+		v.Editable = true
+		v.Wrap = false
+		v.Editor = gocui.EditorFunc(passwordsSearchEditor)
+
+		if _, err := g.SetCurrentView(PasswordsView); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
