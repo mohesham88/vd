864cdb828f6069940d795d0f4cd06a7d4a740035
Ahmed Hossam <ahmedhosssamps@gmail.com>
2026-08-01 00:28:34 +0300

    Increase the time of export feedback msg

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -755,7 +755,7 @@ func handleCommand(g *gocui.Gui) error {
 			return nil
 		}
 
-		showFeedback(g, fmt.Sprintf("Passwords exported to `%s`", filename), 3000)
+		showFeedback(g, fmt.Sprintf("Passwords exported to `%s`", filename), 60)
 	case "/importotp":
 		openMenu("Select source", importProviders, selectImportProvider)
 	case "/change":
